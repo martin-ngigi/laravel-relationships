84ccbb615c3b97288fa263335e7520c01112cb13
martin-ngigi <martinwainaina001@gmail.com>
2023-05-10 08:58:23 +0300

    solved erroe. i.e.  if user has profile and phone, still return data

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,15 +14,34 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'profile_name' => $this->profile->profile_name, //   WORKING ;-)
-            'profile' => $this->profile,
-            'phone_number' => $this->phone->phone_number, //   WORKING ;-)
-            'phone'=>$this->phone,
-            'posts'=>$this->profile->posts,
-        ];
+        if(($this->profile == null) || ($this->phone == null )){
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                // 'profile_name' => $this->profile->profile_name, //   WORKING ;-)
+                'profile' => $this->profile,
+                // 'phone_number' => $this->phone->phone_number, //   WORKING ;-)
+                'phone'=>$this->phone,
+                // 'posts'=>$this->profile->posts,
+            ];
+        }
+        else{
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                'profile_name' => $this->profile->profile_name, //   WORKING ;-)
+                'profile' => $this->profile,
+                'phone_number' => $this->phone->phone_number, //   WORKING ;-)
+                'phone'=>$this->phone,
+                'posts'=>$this->profile->posts,
+            ];
+        }
+
+        //else
+
+        
+       
     }
 }
